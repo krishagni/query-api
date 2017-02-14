f16aa49149741d5380e55fcd299e0e587d830f62
Vinayak Pawar <vinayakapawar@gmail.com>
2017-02-14 22:06:09 +0530

    Moved serialisation and deserialisation of query definition to/from JSON to QueryDef object.

--- src/main/java/com/krishagni/query/domain/QueryDef.java
+++ src/main/java/com/krishagni/query/domain/QueryDef.java
@@ -1,5 +1,10 @@
 package com.krishagni.query.domain;
 
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class QueryDef {
 	private String drivingForm;
 
@@ -57,5 +62,60 @@ public class QueryDef {
 
 	public String getAql(Filter[] conjunctionFilters) {
 		return AqlBuilder.getInstance().getQuery(selectList, filters, conjunctionFilters, expressionNodes);
+	}
+
+	public QueryDef copy() {
+		return QueryDef.fromJson(toJson());
+	}
+
+	public String toJson() {
+		try {
+			return getWriteMapper().writeValueAsString(this);
+		} catch (Exception e) {
+			throw new RuntimeException("Error serialising query def to JSON", e);
+		}
+	}
+
+	public static QueryDef fromJson(String queryDefJson) {
+		QueryDef queryDef = null;
+		try {
+			queryDef = getReadMapper().readValue(queryDefJson, QueryDef.class);
+		} catch (Exception e) {
+			throw new RuntimeException("Error deserialising JSON to query def", e);
+		}
+
+		queryDef.selectList = curateSelectList(queryDef.selectList);
+		return queryDef;
+	}
+
+
+	private static ObjectMapper getReadMapper() {
+		return new ObjectMapper();
+	}
+
+	private static ObjectMapper getWriteMapper() {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.setVisibilityChecker(
+			mapper.getSerializationConfig().getDefaultVisibilityChecker()
+				.withFieldVisibility(JsonAutoDetect.Visibility.ANY)
+				.withGetterVisibility(JsonAutoDetect.Visibility.NONE)
+				.withSetterVisibility(JsonAutoDetect.Visibility.NONE)
+				.withCreatorVisibility(JsonAutoDetect.Visibility.NONE)
+		);
+		return mapper;
+	}
+
+	private static Object[] curateSelectList(Object[] selectList) {
+		Object[] result = new Object[selectList.length];
+		int idx = 0;
+		for (Object field : selectList) {
+			if (field instanceof Map) {
+				field = new ObjectMapper().convertValue(field, SelectField.class);
+			}
+
+			result[idx++] = field;
+		}
+
+		return result;
 	}
 }
